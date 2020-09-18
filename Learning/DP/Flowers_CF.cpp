a571b898a8818ee3fbb25d441f5f2fcba972002d
ashok2811 <ashok281196@gmail.com>
2020-09-18 23:49:38 +0530

    180920

--- Learning/DP/Flowers_CF.cpp
+++ Learning/DP/Flowers_CF.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0);
 typedef long long ll;
+#define mod (1e9 + 7)
 typedef vector<int> vi;
 #define Rep(i, a, b) for (int i=a; i<(b); i++)
 #define trav(a,x) for(auto& a : x)
@@ -19,6 +20,39 @@ typedef vector<int> vi;
 #define all(x) x.begin(), x.end()
 #define ins insert
 
+int k;
+ll pre[100001];
+ll dp[100001];
+int getAns(int n){
+    
+    if(n==0) return 1 ;
+      int ans = 0;
+
+    if(dp[n]!= -1) return dp[n];
+    //W
+    if(n>=k){
+         ans +=getAns(n-k), ans = ans % (int)mod;
+    } 
+   
+    //R
+    ans +=getAns(n-1), ans = ans % (int)mod;
+    return dp[n] = ans;
+}
+
+void init(){
+
+    for (int i = 1; i <=100000; i++)
+    {
+        dp[i] = -1;
+    }
+    
+    for (int i = 1; i <= 100000; i++)
+    {
+        pre[i] = pre[i-1] + getAns(i) , pre[i] = pre[i] % (int)mod;
+    }
+    
+}
+
 int main() {
 
     fastIO
@@ -26,15 +60,13 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    int T; cin >> T;
-    int n , a, b;
+    int T; cin >> T >>k;
+    int a, b;
+    init();
     while (T--) {
         cin >> a >> b ;
-        int sum = 0 ;
-        for (int i = a; i <=b; i++)
-        {
-            sum +=getAns(i);
-        }
+        int sum = (pre[b] - pre[a-1] + (int)mod)%(int)mod; 
+
         cout << sum << nl;
 
     }
