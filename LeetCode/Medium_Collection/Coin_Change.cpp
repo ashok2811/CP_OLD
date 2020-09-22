19efc0a16c8515dc55691c3ed8bf4b64a44ebdbd
ashok2811 <ashok281196@gmail.com>
2020-09-22 23:54:55 +0530

    220920

--- LeetCode/Medium_Collection/Coin_Change.cpp
+++ LeetCode/Medium_Collection/Coin_Change.cpp
@@ -14,20 +14,20 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int dp[amount+1];
-        dp[0] = 0;
-        for(int i = 1 ; i<=amount ; i++){
+        dp[0] = 0; 
+        for(int i = 1 ; i<=amount ; i++){   // Init the dp array with INF
             dp[i] = 1e9;
         }
         
-        for(int i = 1 ; i<=amount ; i++){
-            for(auto x : coins){
-                if((i - x)>= 0){
-                    dp[i] = min(dp[i], dp[i-x]+1);
+        for(int i = 1 ; i<=amount ; i++){  // Go for all amount
+            for(auto x : coins){  // iterate over all coins
+                if((i - x)>= 0){ // Check if is is not out of bound
+                    dp[i] = min(dp[i], dp[i-x]+1); 
                 }
             }
         }
         
-        if(dp[amount] == 1e9){
+        if(dp[amount] == 1e9){  // if still INF, return -1 i.e not possible
             return -1;
         }
         
