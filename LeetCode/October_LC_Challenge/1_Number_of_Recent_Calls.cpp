387a0554de4ef7a0c0d119ad98b24ae3982a5039
ashok2811 <ashok281196@gmail.com>
2020-10-01 21:52:01 +0530

    11020

---
+++ LeetCode/October_LC_Challenge/1_Number_of_Recent_Calls.cpp
@@ -0,0 +1,45 @@
+/*
+You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+
+Implement the RecentCounter class:
+
+    RecentCounter() Initializes the counter with zero recent requests.
+    int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and 
+    returns the number of requests that has happened in the past 3000 milliseconds (including the 
+    new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
+
+It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+
+ 
+
+Example 1:
+
+Input
+["RecentCounter", "ping", "ping", "ping", "ping"]
+[[], [1], [100], [3001], [3002]]
+Output
+[null, 1, 2, 3, 3]
+*/
+
+
+class RecentCounter {
+public:
+    deque<int> rings;
+    RecentCounter() {
+        //
+    }
+    
+    int ping(int t) {
+        rings.push_back(t);
+        while(t - rings.front() > 3000){
+            rings.pop_front();
+        }
+        return (int)rings.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
