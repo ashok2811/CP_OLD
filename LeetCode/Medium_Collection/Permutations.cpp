b1592930658b94305fd81634941ab00e46304e0e
ashok2811 <ashok281196@gmail.com>
2020-10-01 23:05:25 +0530

    11020

--- LeetCode/Medium_Collection/Permutations.cpp
+++ LeetCode/Medium_Collection/Permutations.cpp
@@ -27,17 +27,17 @@ public:
         
         for(int i =0 ; i< nums.size() ; i++){
             vector<int> temp = nums;
-            temp.erase(temp.begin()+i);
-            auto res = permute(temp);
+            temp.erase(temp.begin()+i); // erase the chosen one
+            auto res = permute(temp); // apply the same on the rest of the element
             
-            for(int j = 0 ; j < res.size(); j++){
+            for(int j = 0 ; j < res.size(); j++){  // for each lement in the permutation
                 vector<int> temp1 = res[j];
-                temp1.insert(temp1.begin(), nums[i]);
-                result.push_back(temp1);
+                temp1.insert(temp1.begin(), nums[i]); // add the deleted in the front
+                result.push_back(temp1); // push the final into result
             }
             
         }
-        return result;
+        return result; // return results
         
     }
 };  
